aee0894a8b0db1e56fd503b357d0976212479a4f
Estro13 <yurkagospodarishin12@gmail.com>
2023-07-14 17:32:02 +0300

    add redis

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,6 @@ import 'dotenv/config';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `PORT=${process.env.PORT}, STACKHERO_REDIS_URL_TLS=${process.env.STACKHERO_REDIS_URL_TLS}`;
+    return `PORT=${process.env.PORT}, TEST_EBEXTENSIONS=${process.env.TEST_EBEXTENSIONS}`;
   }
 }
